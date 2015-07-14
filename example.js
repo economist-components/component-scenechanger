ccb3c47850d11c88379a611861e6db0d20a1cc5c
dhounam <donaldhounam@economist.com>
2015-07-14 11:17:48 +0100

    Changes

--- example.js
+++ example.js
@@ -1,4 +1,4 @@
-import ShareBar from './index';
+import SceneChanger from './index';
 import Omniture from '@economist/component-omniture';
 import React from 'react';
 const pageSetting = {
@@ -7,7 +7,7 @@ const pageSetting = {
     pageName: 'This is the pageName',
   },
   Omniture: {
-    position: 'share-bar',
+    position: 'scene-changer',
     page: 'Name of the page',
     hostpage: 'hostpage',
     linkinfo: [ 'the_world_if', 'section', 'article headline' ],
@@ -15,22 +15,22 @@ const pageSetting = {
 };
 export default (
   <div>
-    <h2 style={{ border: 'solid red 1px' }}>Attention this component use .no-touch class by Modernizr</h2>
+    <h2 style={{ border: 'solid red 1px' }}>Attention this component uses .no-touch class by Modernizr</h2>
     <h1>SVG version</h1>
-    <ShareBar {...pageSetting} background="#CCFFDD" />
+    <SceneChanger {...pageSetting} background="#CCFFDD" />
     <hr/>
-    <ShareBar layout="vertical" />
+    <SceneChanger layout="horizontal" />
     <hr/>
     <Omniture />
     <h1>Flip version (Mouseover)</h1>
     <div className="no-touch">
-      <ShareBar useFX={true}
+      <SceneChanger useFX={true}
       fxDirection="flip-to-top"
       fxType="cube"
       background="#333333"
       fxDefaultStateBackground="#999999" />
       <hr/>
-      <ShareBar
+      <SceneChanger
       useFX={true}
       fxDirection="flip-to-bottom"
       fxType="cube"
@@ -39,7 +39,7 @@ export default (
     </div>
     <h1>Touch version</h1>
     <div className="touch">
-      <ShareBar useFX={true}
+      <SceneChanger useFX={true}
       fxDirection="flip-to-top"
       fxType="cube"
       background="#333333"
--- index.js
+++ index.js
@@ -1,173 +1,80 @@
 import React from 'react';
 import Icon from '@economist/component-icon';
 
-export default class ShareBar extends React.Component {
+export default class SceneChanger extends React.Component {
 
   static get propTypes() {
     return {
-      title: React.PropTypes.string,
-      titleTag: React.PropTypes.string,
-      customClass: React.PropTypes.string,
-      Omniture: React.PropTypes.object,
-      s: React.PropTypes.object,
-      layout: React.PropTypes.string,
-      useFX: React.PropTypes.bool,
-      fxDirection: React.PropTypes.string,
-      fxType: React.PropTypes.string,
-      background: React.PropTypes.string,
+      sceneTotal: React.PropTypes.number,
+      sceneIndex: React.PropTypes.number,
+      dotThreshold: React.PropTypes.number,
       icon: React.PropTypes.object,
       icons: React.PropTypes.array,
+      test: React.PropTypes.string
     };
   }
 
   static get defaultProps() {
     return {
-      layout: 'horizontal',
-      background: 'none',
-      useFX: false,
-      fxDirection: '',
-      fxType: '',
+      sceneTotal: 4,
+      sceneIndex: 0,
+      dotThreshold: 300,
       icon: {
         color: '#FFFFFF',
       },
       icons: [
         {
-          href: 'http://www.facebook.com/sharer/sharer.php?u=http://election.economist.com',
-          title: 'Share on Facebook',
-          className: 'fb',
-          type: 'facebook',
-          s: {
-            events: 'event76',
-          },
+          title: 'Left arrow',
+          className: 'la',
+          type: 'left'
         },
         {
-          href: 'https://twitter.com/intent/tweet?url=http://election.economist.com',
-          title: 'Share on Twitter',
-          className: 'twitter',
-          type: 'twitter',
-          s: {
-            events: 'event31',
-          },
-        },
-        {
-          href: 'https://plus.google.com/share?url=http://election.economist.com',
-          title: 'Share on Google Plus',
-          className: 'gplus',
-          type: 'googleplus',
-          s: {
-            events: 'event35',
-          },
-        },
-        {
-          href: 'https://www.linkedin.com/cws/share?url=http://election.economist.com',
-          title: 'Linked In',
-          className: 'linkedin',
-          type: 'linkedin',
-          s: {
-            events: 'event49',
-          },
-        },
-        {
-          href: 'whatsapp://send?text=http://election.economist.com',
-          title: '',
-          className: 'whatsapp',
-          type: 'whatsapp',
-          s: {
-            events: '',
-          },
-        },
-        {
-          href: 'http://www.economist.com/node/21644150/email/',
-          title: 'Email a friend',
-          className: 'mail',
-          type: 'mail',
-          s: {
-            events: 'event81',
-          },
-        },
+          title: 'Right arrow',
+          className: 'ra',
+          type: 'right'
+        }
       ],
+      test: 'Just testing'
     };
   }
 
-  constructor() {
-    super();
-    this.state = { isMobile: 'no-mobile', fxState: 'fxOff' };
-  }
-
   componentWillMount() {
-    if (this.props.title) {
-      this.props.titleTag = <h3>{this.props.title}</h3>;
-    }
-    if (typeof navigator !== 'undefined') {
-      this.setState({
-        isMobile: (navigator.userAgent.match(/Android|iPhone/i) &&
-        !navigator.userAgent.match(/iPod|iPad/i))
-        ? 'mobile' : 'no-mobile',
-      });
-    }
+   console.log("Hello " + this.props.sceneTotal);
   }
 
-  handleClick(icon, event) {
-    if (event.preventDefault) {
-      event.preventDefault();
-    } else {
-      event.returnValue = false;
-    }
-    if (event.target.className === 'mail') {
-      window.open(event.target.getAttribute('href'), '_blank');
-    } else {
-      window.open(event.target.getAttribute('href'),
-      event.target.getAttribute('target'),
-      'scrollbars=1,resizable=1,height=550,width=550');
-    }
-    this.omnitureRegisterClick(event.target, icon);
+  handleClick(index, event) {
+    console.log("Dot " + index);
   }
 
-  omnitureRegisterClick(link, customVars) {
-    const hublinkinfo = this.props.Omniture.linkinfo;
-    const hostpage = this.props.Omniture.hostpage;
-    const linkPosition = this.props.Omniture.position;
-    /* globals s */
-    window.s = Object.assign(s, this.props.s);
-    window.s.linkTrackEvents = window.s.events = 'event97,' + customVars.s.events;
-    window.s.prop45 = hublinkinfo[0] + '>' + linkPosition + '>' + customVars.title;
-    const root = hublinkinfo.shift();
-    window.s.eVar52 = window.s.eVar53 = root + '>' + hostpage + '>' +
-    linkPosition + '>' + hublinkinfo.join('>') + '>' + customVars.title;
-    window.s.tl(window.s.prop45, 'o', window.s.prop45);
-  }
-  /* This functionality is required for touch devices when hover is not triggered */
-  runEffect() {
-    this.setState({ fxState: 'fxOn' });
-  }
 
   render() {
+    var sceneTotal, wrapOpen, wrapClose, content, i, val="Donald";
+    sceneTotal = this.props.sceneTotal;
+    wrapOpen =  <div className="mnv-ec-scenechanger-wrapper">
+                <div className="mnv-ec-scenechanger-alldots-wrapper">
+                ;
+    wrapClose = </div></div>;
+
+    content = [];
+    for (i = 0; i < sceneTotal; i ++) {
+      content.push(
+        <div className="mnv-ec-scenechanger-onedot-wrapper" key={i}>
+          <div className="mnv-ec-scenechanger-dot" onClick={this.handleClick.bind(this,i)}>
+          </div>
+        </div>
+      )
+    }
+
     return (
-      <div className={`mnv-ec-share
-        ${this.props.layout}
-        ${this.state.isMobile}
-        ${this.props.customClass}
-        ${this.props.fxDirection}
-        ${this.props.fxType}
-        ${this.state.fxState}`}>
-        {this.props.titleTag ? this.props.titleTag : null}
-        {this.props.useFX ? <div className="default-state" onClick={this.runEffect.bind(this)}>
-        <Icon type="share" color={this.props.icon.color} background="none" />
-        </div> : null}
-        <div className="mnv-ec-share-icons"
-        style={(this.props.background) ? { background: this.props.background }
-        : null }>
-            {this.props.icons.map((icon, key) => {
-              return (
-              <a key={key} onClick={this.handleClick.bind(this, icon)}
-              href={icon.href} title={icon.title} className={icon.className}
-               target="_blank">
-                <Icon type={icon.type} color={this.props.icon.color} />
-              </a>
-              );
-            })}
+      <div className="mnv-ec-scenechanger-wrapper">
+        <div className="mnv-ec-scenechanger-alldots-wrapper">
+          {content}
         </div>
       </div>
     );
+
   }
 }
+
+//        <Icon type="left"  onClick = {this.handleClick.bind(this)}/>
+//        <Icon type="right"  onClick = {this.handleClick.bind(this)} />
